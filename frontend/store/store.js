3f65ebdb3d5983b5804f83043dede63ab072bff5
RyanGonzalezUSA <mhalim80@hotmail.com>
2019-11-14 12:18:12 -0800

    resolving mixed package manager

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,17 +1,30 @@
 import { createStore, applyMiddleware } from 'redux';
 // import thunk from 'redux-thunk';
 import thunk from '../thunk/thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from '../reducers/root_reducers';
 
-const configureStore = (preloadedState = {}) => (
-  createStore(
-    rootReducer,
-    preloadedState,
-    composeWithDevTools(applyMiddleware(thunk, logger))
-  )
-);
+
+const middleWares = [thunk];
+
+if (process.env.NODE_ENV !== "production") {
+  // must use 'require' (import only allowed at top of file)
+  const { logger } = require("redux-logger");
+  middleWares.push(logger);
+}
+
+const configureStore = (preloadedState = {}) =>
+  createStore(rootReducer, preloadedState, applyMiddleware(...middleWares));
+
+
+// const configureStore = (preloadedState = {}) => (
+//   createStore(
+//     rootReducer,
+//     preloadedState,
+//     composeWithDevTools(applyMiddleware(thunk, logger))
+//   )
+// );
 
 export default configureStore;
