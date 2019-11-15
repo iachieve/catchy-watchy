93ab3f7cad8aca7f08f86542e5ce6aabdf35e451
RyanGonzalezUSA <mhalim80@hotmail.com>
2019-11-14 16:03:39 -0800

    commit message goes here

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,7 @@
 
 require("@rails/ujs").start()
 require("channels")
+require("jquery")
 require('packs/bundle.js')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
